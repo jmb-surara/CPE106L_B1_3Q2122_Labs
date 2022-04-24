71ec8f1835b6c0d501c19130dfc7c00fb9c25e1c
Julian Marc Surara <100174004+jmb-surara@users.noreply.github.com>
2022-04-24 19:07:31 +0800

    Add files via upload

---
+++ Project Sprint 1/UNO Game (Project Sprint 1).py
@@ -0,0 +1,128 @@
+import random
+import time
+
+
+color = ('RED','GREEN','BLUE','YELLOW')
+rank = ('0','1','2','3','4','5','6','7','8','9','Skip','Reverse','Draw2','Draw4','Wild')
+ctype = {'0':'number','1':'number','2':'number','3':'number','4':'number','5':'number','6':'number',
+            '7':'number','8':'number','9':'number','Skip':'action','Reverse':'action','Draw2':'action',
+            'Draw4':'action_nocolor','Wild':'action_nocolor'}
+
+class Card:
+
+    def __init__(self, color, rank):
+        self.rank = rank
+        if ctype[rank] == 'number':
+            self.color = color
+            self.cardtype = 'number'
+        elif ctype[rank] == 'action':
+            self.color = color
+            self.cardtype = 'action'
+        else:
+            self.color = None
+            self.cardtype = 'action_nocolor'
+
+    def __str__(self):
+        if self.color == None:
+            return self.rank
+        else:
+            return self.color + " " + self.rank
+
+
+class Deck:
+
+    def __init__(self):
+        self.deck = []
+        for clr in color:
+            for ran in rank:
+                if ctype[ran] != 'action_nocolor':
+                    self.deck.append(Card(clr, ran))
+                    self.deck.append(Card(clr, ran))
+                else:
+                    self.deck.append(Card(clr, ran))
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return 'The deck has ' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        return self.deck.pop()
+
+
+class Hand:
+
+    def __init__(self):
+        self.cards = []
+        self.cardsstr = []
+        self.number_cards = 0
+        self.action_cards = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.cardsstr.append(str(card))
+        if card.cardtype == 'number':
+            self.number_cards += 1
+        else:
+            self.action_cards += 1
+
+    def remove_card(self, place):
+        self.cardsstr.pop(place - 1)
+        return self.cards.pop(place - 1)
+
+    def cards_in_hand(self):
+        for i in range(len(self.cardsstr)):
+            print(f' {i + 1}.{self.cardsstr[i]}')
+
+    def single_card(self, place):
+        return self.cards[place - 1]
+
+    def no_of_cards(self):
+        return len(self.cards)
+
+
+#Funciton to randomly select who starts first
+def choose_first():
+    if random.randint(0,1)==0:
+        return 'Player'
+    else:
+        return 'Pc'
+
+
+#Function to check if the card thrown by Player/PC is a valid card by comparing it with the top card
+def single_card_check(top_card,card):
+    if card.color==top_card.color or top_card.rank==card.rank or card.cardtype=='action_nocolor':
+        return True
+    else:
+        return False
+
+
+#FOR PC ONLY
+#To check if PC has any valid card to throw 
+def full_hand_check(hand,top_card):
+    for c in hand.cards:
+        if c.color==top_card.color or c.rank == top_card.rank or c.cardtype=='action_nocolor':
+            return hand.remove_card(hand.cardsstr.index(str(c))+1)
+    else:
+        return 'no card'
+
+
+#Function to check if either wins
+def win_check(hand):
+    if len(hand.cards)==0:
+        return True
+    else:
+        return False
+
+
+#Function to check if last card is an action card (GAME MUST END WITH A NUMBER CARD)
+def last_card_check(hand):
+    for c in hand.cards:
+        if c.cardtype!='number':
+            return True
+        else:
+            return False
